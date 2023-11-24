9d2942f53e8f8554def15888056a83d84c78522a
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-24 10:12:32 +0530

    UC8: Multiple Employers

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,25 +2,30 @@ import java.util.*;
 
 public class EmployeeWage {
 	
-	public static final int WAGE_RATE = 20;
-	public static final int WORKING_DAYS = 20;
-	public static final int MAX_WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HOURS = 100;
+	int wageRate;
+	int workingDays;
+	int maxWorkingHours;
+
+	EmployeeWage(int wageRate, int workingDays, int maxWorkingHours){
+		this.wageRate = wageRate;
+		this.workingDays = workingDays;
+		this.maxWorkingHours = maxWorkingHours;
+	}
 
 	int calcDailyWorkHours(int empType){
 		int workHours = 0;
 		switch (empType){
 			case 1:
-					System.out.println("Employee is Present Fulltime");
-					workHours = 8;
-					break;
-				case 2:
-					System.out.println("Employee is Present Part-time");
-					workHours = 2;
-					break;
-				default:
-					workHours = 0;
-					System.out.println("Employee is Absent");
+				System.out.println("Employee is Present Fulltime");
+				workHours = 8;
+				break;
+			case 2:
+				System.out.println("Employee is Present Part-time");
+				workHours = 2;
+				break;
+			default:
+				workHours = 0;
+				System.out.println("Employee is Absent");
 			}
 		return workHours;
 	}
@@ -33,18 +38,24 @@ public class EmployeeWage {
 		int totalWorkHours = 0;
 		int totalPresentDays = 0;
 		
-		while(totalWorkHours<=MAX_WORKING_HOURS && totalPresentDays<=MAX_WORKING_DAYS){
+		while(totalWorkHours<=maxWorkingHours && totalPresentDays<=workingDays){
 			totalPresentDays++;
 			double empType = Math.floor(Math.random() * 10) % 3;
 			workHours = calcDailyWorkHours((int)empType);
 			totalWorkHours += workHours;
 		}
 		
-		empWage = totalWorkHours * WAGE_RATE;
+		empWage = totalWorkHours * wageRate;
 		System.out.println("Total Employee Wage for the month is: "+empWage);
 	}
 	public static void main(String[] args) {
-		EmployeeWage E1 = new EmployeeWage();
+		EmployeeWage E1 = new EmployeeWage(20, 20, 100);
+		EmployeeWage E2 = new EmployeeWage(22, 20, 120);
+		// EmployeeWage E3 = new EmployeeWage(25, 26, 80);
+
+		System.out.println("Company 1\n");
 		E1.calculateTotalWage();
+		System.out.println("Company 1\n");
+		E2.calculateTotalWage();
 	}
 }
