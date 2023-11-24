ba8e353a9e40a0398715437d2b448e5e69a34928
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-24 08:18:07 +0530

    UC7: Use Class Method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,19 +7,10 @@ public class EmployeeWage {
 	public static final int MAX_WORKING_DAYS = 20;
 	public static final int MAX_WORKING_HOURS = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Employee Attendance");
-		
-		int empWage = 0;
+	int calcDailyWorkHours(int empType){
 		int workHours = 0;
-		int totalWorkHours = 0;
-		int totalPresentDays = 0;
-		
-		while(totalWorkHours<=MAX_WORKING_HOURS && totalPresentDays<=MAX_WORKING_DAYS){
-			totalPresentDays++;
-			double empType = Math.floor(Math.random() * 10) % 3;
-			switch ((int)empType){
-				case 1:
+		switch (empType){
+			case 1:
 					System.out.println("Employee is Present Fulltime");
 					workHours = 8;
 					break;
@@ -31,10 +22,29 @@ public class EmployeeWage {
 					workHours = 0;
 					System.out.println("Employee is Absent");
 			}
+		return workHours;
+	}
+
+	void calculateTotalWage(){
+		System.out.println("Employee Attendance");
+		
+		int empWage = 0;
+		int workHours = 0;
+		int totalWorkHours = 0;
+		int totalPresentDays = 0;
+		
+		while(totalWorkHours<=MAX_WORKING_HOURS && totalPresentDays<=MAX_WORKING_DAYS){
+			totalPresentDays++;
+			double empType = Math.floor(Math.random() * 10) % 3;
+			workHours = calcDailyWorkHours((int)empType);
 			totalWorkHours += workHours;
 		}
 		
 		empWage = totalWorkHours * WAGE_RATE;
 		System.out.println("Total Employee Wage for the month is: "+empWage);
+	}
+	public static void main(String[] args) {
+		EmployeeWage E1 = new EmployeeWage();
+		E1.calculateTotalWage();
 	}
 }
